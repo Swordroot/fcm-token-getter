9383823541fbd512433da62ca942784782f25e73
刀根諒 <tstarouimnpkegto@gmail.com>
2021-10-10 18:04:06 +0900

    Initial Commit

---
+++ Shared/timeschedulertestApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeschedulertestApp.swift
+//  Shared
+//
+//  Created by Makoto Tone on 2021/10/10.
+//
+
+import SwiftUI
+
+@main
+struct timeschedulertestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
