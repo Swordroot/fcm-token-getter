71748cbac2a8729f8eb45c58cdee44e07bc2e46e
刀根諒 <tstarouimnpkegto@gmail.com>
2021-10-30 12:36:22 +0900

    FCMトークン取得サンプル作成

--- Shared/timeschedulertestApp.swift
+++ Shared/timeschedulertestApp.swift
@@ -1,17 +1,117 @@
-//
-//  timeschedulertestApp.swift
-//  Shared
-//
-//  Created by Makoto Tone on 2021/10/10.
-//
+import UIKit
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
-import SwiftUI
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-@main
-struct timeschedulertestApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        if #available(iOS 10.0, *) {
+           // For iOS 10 display notification (sent via APNS)
+           UNUserNotificationCenter.current().delegate = self
+
+           let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+           UNUserNotificationCenter.current().requestAuthorization(
+               options: authOptions,
+               completionHandler: {_, _ in })
+        } else {
+           let settings: UIUserNotificationSettings =
+               UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+           application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // Print message ID.
+        if let messageID = userInfo["gcm.message_id"] {
+           print("Message ID: \(messageID)")
+        }
+
+        // Print full message.
+        print(userInfo)
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // Print message ID.
+        if let messageID = userInfo["gcm.message_id"] {
+           print("Message ID: \(messageID)")
+        }
+
+        // Print full message.
+        print(userInfo)
+
+        completionHandler(UIBackgroundFetchResult.newData)
+    
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        var readableToken: String = ""
+        for i in 0..<deviceToken.count {
+            readableToken += String(format: "%02.2hhx", deviceToken[i] as CVarArg)
+        }
+        print("Received an APNs device token: \(readableToken)")
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
         }
     }
+    func application(_ application: UIApplication, didRecieveRegistreToken fcmToken: String) {
+        
+    }
+
+
+}
+
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+   func userNotificationCenter(_ center: UNUserNotificationCenter,
+                               willPresent notification: UNNotification,
+                               withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+       let userInfo = notification.request.content.userInfo
+
+       if let messageID = userInfo["gcm.message_id"] {
+           print("Message ID: \(messageID)")
+       }
+
+       print(userInfo)
+
+       completionHandler([])
+   }
+
+   func userNotificationCenter(_ center: UNUserNotificationCenter,
+                               didReceive response: UNNotificationResponse,
+                               withCompletionHandler completionHandler: @escaping () -> Void) {
+       let userInfo = response.notification.request.content.userInfo
+       if let messageID = userInfo["gcm.message_id"] {
+           print("Message ID: \(messageID)")
+       }
+
+       print(userInfo)
+
+       completionHandler()
+   }
 }
